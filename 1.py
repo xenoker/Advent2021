309ff2bb7c0768cfa3da1f01d595b1235ee83228
Xenoker <xenoker@users.noreply.github.com>
2021-12-01 01:10:35 -0500

    Initial commit

---
+++ 1.py
@@ -0,0 +1,12 @@
+from puzzleinput import getints
+INTS = getints(1)
+
+def part1():
+    return sum(b>a for a,b in zip(INTS,INTS[1:]))
+def part2():
+    return sum(sum(INTS[i+1:][:3])>sum(INTS[i:][:3]) for i in range(len(INTS)-3))
+
+if __name__ == '__main__':
+    assert part1()==1342
+    assert part2()==1378
+
