10eb77bf1139c12be80b58922ac7518c46e64ce2
Xenoker <xenoker@users.noreply.github.com>
2021-12-07 01:04:55 -0500

    Day7

---
+++ 7.py
@@ -0,0 +1,22 @@
+from puzzleinput import get
+from functools import lru_cache
+CRABS = list(map(int,get(7).split(',')))
+s_crabs = set(CRABS)
+c_crabs = dict((k,CRABS.count(k)) for k in s_crabs)
+
+@lru_cache(maxsize = len(c_crabs)*2)
+def fuelcost(pn,diff):
+    if pn==1: return diff
+    return sum(range(1,diff+1))
+
+def part(pn):
+    return min( sum(fuelcost(pn,abs(p-pos))*n for p,n in c_crabs.items()) for pos in range(min(s_crabs),max(s_crabs)) )
+
+if __name__ == '__main__':
+    assert part(1)==349769
+    assert part(2)==99540554
+
+
+    
+    
+
